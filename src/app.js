10c4fdb9d7e9587b3cab812f1db08ff421f8b4aa
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-05-22 16:28:42 +0100

    Implement `GET /contracts` endpoint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { Op } = require("sequelize");
 const { sequelize } = require("./model");
 const { getProfile } = require("./middleware/getProfile");
 
@@ -8,8 +9,20 @@ app.use(bodyParser.json());
 app.set("sequelize", sequelize);
 app.set("models", sequelize.models);
 
+app.get("/contracts", getProfile, async (req, res) => {
+  const { Contract } = req.app.get("models");
+  const profileId = req.profile.get("id");
+  const contracts = await Contract.findAll({
+    where: {
+      [Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }],
+      status: { [Op.not]: "terminated" },
+    },
+  });
+
+  return res.json(contracts);
+});
+
 /**
- * FIX ME!
  * @returns contract by id
  */
 app.get("/contracts/:id", getProfile, async (req, res) => {
--- src/app.spec.js
+++ src/app.spec.js
@@ -10,11 +10,59 @@ beforeEach(async () => {
 
 describe("on GET /contracts", () => {
   it("should fail with 400 if profile_id header not set", async () => {
-    await request.get("/contracts").expect(400);
+    await request.get("/contracts").expect(401);
   });
 
-  it("should return a list of contracts belonging to a profile_id", async () => {
-    await request.get("/contracts").expect(200);
+  it("should return a list of contracts belonging to a client", async () => {
+    const response = await request.get("/contracts").set("profile_id", 3);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([
+      {
+        ClientId: 3,
+        ContractorId: 8,
+        createdAt: expect.any(String),
+        id: 5,
+        status: "new",
+        terms: "bla bla bla",
+        updatedAt: expect.any(String),
+      },
+      {
+        ClientId: 3,
+        ContractorId: 7,
+        createdAt: expect.any(String),
+        id: 6,
+        status: "in_progress",
+        terms: "bla bla bla",
+        updatedAt: expect.any(String),
+      },
+    ]);
+  });
+
+  it("should return a list of contracts belonging to a contractor", async () => {
+    const response = await request.get("/contracts").set("profile_id", 8);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual([
+      {
+        ClientId: 3,
+        ContractorId: 8,
+        createdAt: expect.any(String),
+        id: 5,
+        status: "new",
+        terms: "bla bla bla",
+        updatedAt: expect.any(String),
+      },
+      {
+        ClientId: 4,
+        ContractorId: 8,
+        createdAt: expect.any(String),
+        id: 9,
+        status: "in_progress",
+        terms: "bla bla bla",
+        updatedAt: expect.any(String),
+      },
+    ]);
   });
 });
 
