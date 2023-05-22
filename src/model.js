6ba49103e80f8b3938747dd4e8b16645ba4b9e54
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2023-05-22 15:30:01 +0100

    Improve models/seed for testing

--- src/model.js
+++ src/model.js
@@ -2,7 +2,9 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite3'
+  storage: process.env.NODE_ENV === 'test'
+      ? ':memory:'
+      : './database.sqlite3'
 });
 
 class Profile extends Sequelize.Model {}
